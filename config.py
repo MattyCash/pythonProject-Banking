becaabe10ca8f9ad4064cd25b24d9583fc862897
Adil <adil.orazalin03@gmail.com>
2023-12-17 14:46:40 +0600

    Minor Changes

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-api_key = '758c59663d57819d8844ca47d3656ea8'
-api_endpoint = f'https://v6.exchangerate-api.com/v6/603b05e22e866bc0d33cb4f0/latest/'
+api_key = '603b05e22e866bc0d33cb4f0'
+api_endpoint = f'https://open.er-api.com/v6/latest?apikey=603b05e22e866bc0d33cb4f0'
